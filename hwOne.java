aa6509268a23f197e1c636102960a7ae722f9a61
Maximum Wilder-Smith <maxwildersmith@gmail.com>
2020-01-23 15:07:08 -0800

    Changed java file to say my name

--- hwOne.java
+++ hwOne.java
@@ -1,6 +1,6 @@
 
 public class main {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        System.out.println("Hello, my name is Maximum Wilder-Smith");
     }
 }
